28cef7bc9cd63b3105a20e545c3315037e3f86e8
thien06012001 <thien06012001@gmail.com>
2022-11-13 10:36:34 +0700

    Initial commit

--- pages/HomeScreen.tsx
+++ pages/HomeScreen.tsx
@@ -3,7 +3,23 @@ import { useSelector } from 'react-redux'
 import { auth } from '../firebase'
 import { selectUser } from '../slices/userSlice'
 function HomeScreen() {
+    const info : any= 'User'
     const user = useSelector(selectUser)
+    if (user !== null) {
+        // The user object has basic properties such as display name, email, etc.
+        const displayName = user.displayName;
+        const email = user.email;
+        const emailVerified = user.emailVerified;
+      
+        // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+        const uid = user.uid;
+    }
+    else{
+        const displayName = 'error';
+        const email = 'error';
+    }
     return (
     <div>
         <h1>Welcome to our page</h1>
