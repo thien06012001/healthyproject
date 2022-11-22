28ff9403cddc1763bf8481834f6e1fff54073d72
thien06012001 <thien06012001@gmail.com>
2022-11-22 17:08:12 +0700

    update

--- components/PeopleSay.tsx
+++ components/PeopleSay.tsx
@@ -6,17 +6,18 @@ type Props = {}
 
 function PeopleSay({}: Props) {
   return (
-    <div className='flex flex-row ml-[200px]'>
+    <div className='flex flex-row '>
       <div>
         <div className='w-[415px] h-[128px]'>
           <p className='text-[48px] text-[#333333]'>What Are People <br /> 
           Saying <span className='text-[#FB9333] text-[48px]'>About Us</span></p> 
         </div>
-        <div className='w-[400px] h-[72px] '>
+        <div className='w-[400px] h-[72px] mt-[50px] '>
           <p className='text-[16px] text-[#727272]'>We are very happy if you are satisfied with our service and products, 
             let's read pure reviews from customers who bought our products.</p>
         </div>
       </div>
+      <div></div>
     </div>
   )
 }
