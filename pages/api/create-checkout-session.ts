6b548e290fe6a8f3b67124bf2c78142a87e8353f
thien06012001 <thien06012001@gmail.com>
2023-01-12 19:43:37 +0700

    update

--- pages/api/create-checkout-session.ts
+++ pages/api/create-checkout-session.ts
@@ -31,7 +31,11 @@ export default async (req:NextApiRequest, res:NextApiResponse) =>{
             images: JSON.stringify(items.map((item:any) => item.image))
         }
     })
-    res.status(200).json({id: session.id})
+    // res.status(200).json({id: session.id})
+    setTimeout(() => {
+        // 3. Notify Stripe that event recieved.
+        res.json({id: session.id})
+    }, 2000)
 }
 
 
